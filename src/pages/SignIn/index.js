d77610c8c8cd8e0aecf68ba55bb85c88a0aa1f96
DiegoXavier-hub <diegohenrique.mail@gmail.com>
2024-03-31 13:17:33 -0300

    finalização da estilização de login e register

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 import Fire from '../../assets/images/fire.jpg'
+import '../../assets/sass/signIn.css'
 
 export default function SignIn() {
 
@@ -8,20 +9,23 @@ export default function SignIn() {
     const [password, setPassword] = useState('')
 
     return (
-        <div className="SignIn">
+        <div id="SignIn">
             <div className="container">
                 <img src={Fire} alt="fireImage"/>
                 <form>
                     <h1>Hello Again!</h1>
-                    <span>Welcome back you've been missed!</span>
+                    <h2>Welcome back you've been missed!</h2>
 
                     <input type="text" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} required/>
-                    <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
+                    <span className='email-console'>&nbsp;</span>
+                    <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} 
+                    required/>
+                    <span className='password-console'>&nbsp;</span>
 
-                    <a href='/'>Recovery Password</a>
+                    <Link to='/' className='recovery-password'>Recovery Password</Link>
                     <button type='submit'>Sign In</button>
+                    <div className='register-link-text'>Not a member?<Link to='/signUp' className='register-link'> Register now</Link></div>
                 </form>
-                    <div>Not a member?<Link to='/signUp'> Register now</Link></div>
             </div>
         </div>
     );
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,35 @@
-export default function SignUp() {
+import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom'
+import Fire from '../../assets/images/flame.jpg'
+import '../../assets/sass/signUp.css'
+
+export default function SignIn() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+
     return (
-        <div className="SignUp">
-            <h1>Sign Up</h1>
+        <div id="SignUp">
+            <div className="container">
+                <img src={Fire} alt="fireImage"/>
+                <form>
+                    <h1>Join Us</h1>
+                    <h2>Welcome it's nice to see you here!</h2>
+
+                    <input type="text" value={name} placeholder="Name" onChange={(e) => setName(e.target.value)} required/><span className='email-console'>&nbsp;</span>
+
+                    <input type="text" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} required/>
+                    <span className='email-console'>&nbsp;</span>
+
+                    <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} 
+                    required/>
+                    <span className='password-console'>&nbsp;</span>
+
+                    <button type='submit'>Sign In</button>
+                    <div className='register-link-text'>Already a member?<Link to='/signIn' className='register-link'> Sign in</Link></div>
+                </form>
+            </div>
         </div>
     );
 }
